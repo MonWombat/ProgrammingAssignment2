2fe2eeaf8c4c1bc91c4c0f58c92fff8205269ca7
Monika <jakubczakmonika@gmail.com>
2015-02-22 23:10:00 +1100

    cachematrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,8 @@
+#makeCasheMatrix is a function which is comparable to a class object
+#it has 4 member functions which are comparable to methods
+# member functions are:set, get, setinverse and getinverse
+# <<- assignment operator causes a search to made through parent environments for an existing definition of the variable being assigned
+
 makeCacheMatrix <- function(x = matrix()) {
   InvMat <- NULL ## initializes variable InvMat to store the result of inverse matrix
   set <- function(y) ## set function sets the value of the input matrix
@@ -9,8 +14,14 @@ makeCacheMatrix <- function(x = matrix()) {
   setinverse <- function(inverse) InvMat <<- inverse #sets the inverse matrix 
   getinverse <- function() InvMat #returns the inverse matrix
   list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
-  # list returns all the objects/functions created in makeCacheMatrix function
+  # returns all the objects/functions created in makeCacheMatrix function
 }
+
+#cacheSolve calculates the inverse of matrix created with makeCacheMatrix function
+#first it checks if the inverse matrix already exists
+#if it does then it gets the inverse from cache and skips calculations
+#if it doesn't, it calculates the inverse of the inupt matrix using solve() function
+#then it sets the inverse matrix in the cache using setinverse() function
 
 cacheSolve <- function(x, ...) {
   # Return an inverse matrix of the input matrix x
